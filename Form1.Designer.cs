7b24cb6ee9977ec4474fb27a00c5dd32f43edec0
Philip Graham <GrahPhil963@GoTVDSB.ca>
2022-03-07 10:07:39 -0500

    I changed the size of the button

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,20 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnSubmit = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(578, 156);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 50);
+            this.btnSubmit.TabIndex = 0;
+            this.btnSubmit.Text = "Click Me ";
+            this.btnSubmit.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSubmit);
             this.Name = "MainForm";
-            this.Text = "Form1";
+            this.Text = "Getting Input";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnSubmit;
     }
 }
 
